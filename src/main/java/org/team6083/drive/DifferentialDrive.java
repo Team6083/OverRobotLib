e52ba3ec6d35ba109e7f58e35e7ab8c353c5574f
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-12-10 22:47:35 +0800

    add from 2018 code

--- src/main/java/org/team6083/drive/DifferentialDrive.java
+++ src/main/java/org/team6083/drive/DifferentialDrive.java
@@ -1,6 +1,7 @@
 package org.team6083.drive;
 
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 /**
@@ -10,6 +11,8 @@ import edu.wpi.first.wpilibj.SpeedController;
  */
 public class DifferentialDrive {
     private SpeedController leftMotor1, leftMotor2, rightMotor1, rightMotor2;
+    private boolean reverseDrive = false;
+    private double speedDown;
 
     /**
      * Construct a DifferentialDrive.
@@ -23,16 +26,21 @@ public class DifferentialDrive {
         leftMotor2 = left2;
         rightMotor1 = right1;
         rightMotor2 = right2;
+
+        speedDown = 2.0;
+        dashboard(0,0);
     }
 
     /**
-     * Stop the driv e
+     * Stop the drive
      */
     public void stop() {
         leftMotor1.set(0);
         rightMotor1.set(0);
         leftMotor2.set(0);
         rightMotor2.set(0);
+
+        dashboard(0,0);
     }
 
     /**
@@ -46,7 +54,30 @@ public class DifferentialDrive {
         leftMotor2.set(leftSpeed);
         rightMotor1.set(rightSpeed);
         rightMotor2.set(rightSpeed);
+
+        dashboard(leftSpeed,rightSpeed);
     }
 
+    /**
+     * Get speed of left drive.
+     * @return The set value of left drive.
+     */
+    public double getLeftPower() {
+        return leftMotor1.get();
+    }
 
+    /**
+     * Get speed of right speed.
+     * @return The set value of right drive.
+     */
+    public double getRightPower() {
+        return rightMotor1.get();
+    }
+
+    private void dashboard(double l_speed, double r_speed){
+        SmartDashboard.putBoolean("drive/reverse", reverseDrive);
+        SmartDashboard.putNumber("drive/leftSpeed", l_speed);
+        SmartDashboard.putNumber("drive/rightSpeed", r_speed);
+        SmartDashboard.putNumber("drive/speedDown", speedDown);
+    }
 }
