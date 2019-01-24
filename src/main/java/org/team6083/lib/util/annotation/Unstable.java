2e9d4f191815c4d5f8be63f99f37964dd4f8e05c
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-01-24 14:04:00 +0800

    add unstable announcement

---
+++ src/main/java/org/team6083/lib/util/annotation/Unstable.java
@@ -0,0 +1,10 @@
+package org.team6083.lib.util.annotation;
+
+import java.lang.annotation.Documented;
+
+/**
+ * No guarantee is provided as to reliability or stability across any level of release granularity.
+ */
+@Documented
+public @interface Unstable {
+}
