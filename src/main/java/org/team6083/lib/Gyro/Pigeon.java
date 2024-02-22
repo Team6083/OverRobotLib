0b0d8a970816c864831a01fe042e6ac78d67fee1
AYao1026 <95smoonplasticworkuse@gmail.com>
2024-02-22 19:33:05 +0800

    [modify]Pigeon

--- src/main/java/org/team6083/lib/Gyro/Pigeon.java
+++ src/main/java/org/team6083/lib/Gyro/Pigeon.java
@@ -2,18 +2,19 @@ package org.team6083.lib.Gyro;
 
 import org.team6083.lib.drive.DriveGyro;
 
+import com.ctre.phoenix6.hardware.Pigeon2;
+
 public class Pigeon extends Pigeon2 implements DriveGyro  {
 public Pigeon pigeon= new Pigeon();
     @Override
     public double getHeading() {
-        // TODO Auto-generated method stub
         pigeon.getHeading();
         throw new UnsupportedOperationException("Unimplemented method 'getHeading'");
     }
 
     @Override
     public void reset() {
-        // TODO Auto-generated method stub
+
         pigeon.reset();
         throw new UnsupportedOperationException("Unimplemented method 'reset'");
     }
