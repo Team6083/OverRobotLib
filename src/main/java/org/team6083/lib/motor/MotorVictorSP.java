48506114e31dac92624bc8eae1dc2ef4240955b2
Hannahjjj97 <hannahjjj97@gmail.com>
2024-02-28 14:02:45 +0800

    [add]Motor

--- src/main/java/org/team6083/lib/motor/MotorVictorSP.java
+++ src/main/java/org/team6083/lib/motor/MotorVictorSP.java
@@ -1,14 +1,38 @@
 package org.team6083.lib.motor;
 
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
-public class MotorVictorSP extends edu.wpi.first.wpilibj.motorcontrol.VictorSP implements MotorController{
+public class MotorVictorSP extends edu.wpi.first.wpilibj.motorcontrol.VictorSP{
     public MotorVictorSP(int deviceId){
        super(deviceId);
+    }   
+    @Override
+    public double get() {
+        return super.get();
     }
-    public void set(double speed, double kp){
-        super.set(speed*kp);
+
+    @Override
+    public void setInverted(boolean isInverted) {
+        super.setInverted(isInverted);
     }
-    
+
+    @Override
+    public boolean getInverted() {
+        return super.getInverted();
+    }
+
+    @Override
+    public void disable() {
+        super.set(0);
+    }
+
+    @Override
+    public void stopMotor() {
+        super.set(0);
+    }
+
+    @Override
+    public void set(double speed) {
+        super.set(speed);
+    }
 
 }
