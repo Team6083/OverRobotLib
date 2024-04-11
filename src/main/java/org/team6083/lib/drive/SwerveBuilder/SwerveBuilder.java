64ca202f789313e6282ab8a3b5985195b443fdc8
Hannahjjj97 <hannahjjj97@gmail.com>
2024-04-11 13:35:48 +0800

    [add]SwerveBuilder

---
+++ src/main/java/org/team6083/lib/drive/SwerveBuilder/SwerveBuilder.java
@@ -0,0 +1,11 @@
+package org.team6083.lib.drive.SwerveBuilder;
+public class SwerveBuilder {
+    public SwerveBuilder{
+            driveMotorID[];
+            MotorController driveMotor, MotorController turningMotor,
+            RobotEncoder driveEncoder, RobotEncoder turningEncoder,
+            double kPRot, double kIRot, double kDRot,
+            double kDesireSpeedToMotorVoltage
+    }
+    
+}
